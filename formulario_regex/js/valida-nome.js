93dcabce3111afe145c2c37c00ecb5f08d592658
camilapessoa <camilabentespessoa@gmail.com>
2023-10-03 06:34:44 -0300

    Vídeo 5.2 - Antecipação negativa

--- formulario_regex/js/valida-nome.js
+++ formulario_regex/js/valida-nome.js
@@ -1,6 +1,6 @@
 export default function ehUmNome(campo) {
     const nome = campo.value
-    const patternNome = /^[A-Za-zÀ-ÿ -]{3,30}$/.test(nome)
+    const patternNome = /^(?!(.)\1\1)[A-Za-zÀ-ÿ -]{3,30}$/i.test(nome)
     console.log(patternNome);
 
     if (!patternNome) {
