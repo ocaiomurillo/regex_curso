7c0a13271eaf4087a80a11d6f3c204a729ac1e97
camilapessoa <camilabentespessoa@gmail.com>
2023-10-03 05:10:35 -0300

    Vídeo 3.3 - Configurando grupos de não captura

--- buscas_csv/busca_nome.js
+++ buscas_csv/busca_nome.js
@@ -3,7 +3,7 @@ const bancoCsv = 'database.csv'
 const banco = fs.readFileSync(bancoCsv, "utf-8")
 
 
-const patternNomes = /^([A-Za-zÀ-ÿ]+)(\s[A-Za-zÀ-ÿ]+)+/gm
+const patternNomes = /^([A-Za-zÀ-ÿ]+)(?:\s([A-Za-zÀ-ÿ]+))+/gm
 
 
 const matchNomes = banco.match(patternNomes)
