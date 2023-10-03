2f9efa4a0de08e2a2063fd6d65427975e3955a51
camilapessoa <camilabentespessoa@gmail.com>
2023-10-03 04:48:45 -0300

    Video 2.1 quantificadores em buscas de telefone

--- buscas_csv/busca_telefone.js
+++ buscas_csv/busca_telefone.js
@@ -2,9 +2,12 @@ const fs = require('fs')
 const bancoCsv = 'database.csv'
 const banco = fs.readFileSync(bancoCsv, "utf-8")
 
-//const regexTelefone = /\d+/g
-
-const regexTelefone = /\(\d+\)\s\d+-\d+/g
+const regexTelefone = /\(\d{2}\)\s\d{4,5}-\d{4}/g
 
 const matchTelefone = banco.match(regexTelefone)
 console.log(matchTelefone);
+
+const patternCel = /\(\d{2}\)\s\d{5}-\d{4}/g
+
+const matchCelular = banco.match(patternCel)
+console.log(matchCelular);
